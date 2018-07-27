8842f5712bda3024ee5faa05ffa8d31a8932c81a
42 Robotics <42robotics@42s-iMac-10.local>
2018-07-27 11:46:20 -0700

    more

---
+++ volume.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int	highest(int*arr, int size)
+{
+	int idx = 0;
+	int i = 0;
+	while (i < size)
+	{
+		if(arr[i] > arr[idx])
+			idx=i;
+		i++;
+	}
+	return(idx);
+}
+
+int    volume_histogram(int *arr, int size)
+{
+	int water_lvl = arr[0];
+	int water_volume =0;
+	int h = highest(arr, size);
+	int i = 0;
+	while (i < h)
+	{
+		if (arr[i] < water_lvl)
+			water_volume += water_lvl - arr[i];
+		if (arr[i] > water_lvl)
+			water_lvl = arr[i];
+		i++;
+	}
+	water_lvl = arr[size-1];
+	i = size-1;
+	while (i > h)
+	{
+		if (arr[i] < water_lvl)
+			water_volume += water_lvl - arr[i];
+		if (arr[i] > water_lvl)
+			water_lvl = arr[i];
+		i--;
+
+	}
+	return(water_volume);
+
+}
+
+int main()
+{
+	int arr[] = {0, 0, 4, 0, 0, 6, 0, 0, 3, 0, 5, 0, 1, 0, 0, 0};
+	int h = volume_histogram(arr, 16);
+	printf("%d\n", h);
+	return (0);
+}
+
+
+
+jupyter-notebook --no-browser --port 8888 --ip=0.0.0.0 --config NotebookApp.password='' --config NotebookApp.token='' --config NotebookApp.password_required=False
+
