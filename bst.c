227785c0ab79f89926ba651ea899380104ff5cf8
white_rabbit <dariagurova91@gmail.com>
2019-01-16 21:20:02 -0800

    Update bst.c

--- bst.c
+++ bst.c
@@ -2,153 +2,127 @@
 #include <stdlib.h>
 
 struct s_node {
-        int           value;
-        struct s_node *right;
-        struct s_node *left;
-    };
+		int           value;
+		struct s_node *right;
+		struct s_node *left;
+	};
 
-typedef struct s_node node;
+typedef struct s_node tree;
 
-node *init(int val)
+tree *init(int val)
 {
-	node *n = malloc(sizeof(node));
+	tree *n = malloc(sizeof(tree));
 	n->value = val;
-	n->left = NULL;
 	n->right = NULL;
-	return (n);
+	n->left = NULL;
+	return(n);
 }
 
-node *add_l(node *root, int val)
+tree *addL(tree *root, int val)
 {
-	node *n = init(val);
+	tree *n = init(val);
 	root->left = n;
-	return (n);
+	return(n);
 }
 
-node *add_r(node *root, int val)
+tree *addR(tree *root, int val)
 {
-	node *n = init(val);
+	tree *n = init(val);
 	root->right = n;
-	return (n);
+	return(n);
 }
 
-void	printer(node *root, int level)
-{
-	printf("value: %d\n", root->value);
-	if (root->left)
-	{
-		printf("left ->");
-		printer(root->left, level+1);
-	}
-	else
-		printf("left is empty\n");
-	if (root->right)
-	{
-		printf("right ->");
-		printer(root->right, level+1);
-	}
-	else
-		printf("right is empty\n");
-	printf("level up\n");
-
-}
-
-int is_leaf(node *root)
+int isLeaf(tree *root)
 {
 	if (!root->left && !root->right)
-		return (1);
+		return(1);
 	return(0);
 }
 
-node *smallest(node *root)
+tree *descen(tree *root)
 {
 	if (!root)
-		return (NULL);
+		return(NULL);
 	if (!root->left)
-		return (NULL);
+		return(NULL);
 	if (!root->left->left)
-		return (root);
-	return (smallest(root->left));
+		return(root);
+	return(descen(root->left));
 }
 
-node *bst(node *root)
+tree *convert_bst(tree *root)
 {
-	node *depot = NULL;
-	while (!is_leaf(root))
+	if (!root)
+		return(NULL);
+	tree *res = NULL;
+	while (!isLeaf(root))
 	{
-		
-		node* sm;
-		node *p = smallest(root);
-		if (p)
+		tree *small;
+		tree *pre = descen(root);
+		if (pre)
 		{
-			sm=p->left;
-			p->left = sm->right;
+			small = pre->left;
+			pre->left = small->right;
 		}
 		else
 		{
-			sm = root;
+			small = root;
 			root = root->right;
 		}
-		sm->left = depot;
-		depot = sm;
-		sm->right = NULL;
+		small->left = res;
+		res = small;
+		small->right = NULL;
 	}
-	root->left = depot;
-	depot = root;
 
-	node *tmp = depot;
+	root->left = res;
+	res = root;
+
+
+	tree *tmp = res;
 	while (tmp->left)
 	{
-		node* l= tmp->left;
-		l->right=tmp;
-
+		tree *l = tmp->left;
+		l->right = tmp;
 		tmp = tmp->left;
 	}
-	tmp->left=depot;
-	depot->right = tmp;
-	//printer(depot,0);
-	return (depot);
+
+	tmp->left = res;
+	res->right = tmp;
+
+	return(tmp);
+
 }
 
-void print(node *depot)
+void	print(tree *root)
 {
-	node* start=depot;
-	depot = depot->left;
-	while (depot!=start)
+	tree *start = root;
+	printf("%d\n",start->value);
+	root = root->right;
+
+	while(root != start)
 	{
-		printf("%d\n", depot->value);
-
-		depot = depot->left;
+		printf("%d\n", root->value);
+		root = root->right;
 	}
-	//printf("%d\n", depot->value);
 }
-
-
-
 
 int main ()
 {
-	node *root = init(8);
-	node *n3 = add_l(root,3);
-	add_l(n3,1);
-	node *n6 = add_r(n3,6);
-	add_l(n6, 4);
-	add_r(n6, 7);
-	node *n10 = add_r(root,10);
-	node *n14 = add_r(n10, 14);
-	add_l(n14,13);
+	tree *root = init(8);
+	tree *n3 = addL(root,3);
+	addL(n3,1);
+	tree *n6 = addR(n3,6);
+	addL(n6, 4);
+	addR(n6, 7);
+	tree *n10 = addR(root,10);
+	tree *n14 = addR(n10, 14);
+	addL(n14,13);
 
-	node *sm = bst(root);
+	tree *sm = convert_bst(root);
 	print(sm);
 
-	//printer(root, 0);
 	return (0);
 }
-
-
-
-
-
 
 
 
